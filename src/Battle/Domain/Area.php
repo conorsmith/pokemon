4becd335c13dd1b0731c5d21ec68ec1f16ed9416
Conor Smith <conorjbsmith@gmail.com>
2023-03-28 22:41:54 +0100

    More work

--- src/Battle/Domain/Area.php
+++ src/Battle/Domain/Area.php
@@ -3,8 +3,31 @@ declare(strict_types=1);
 
 namespace ConorSmith\Pokemon\Battle\Domain;
 
+use ConorSmith\Pokemon\ItemId;
+
 final class Area
 {
+    private const DEFAULT_PRIZES = [
+        ItemId::BLACK_BELT,
+        ItemId::BLACK_GLASSES,
+        ItemId::CHARCOAL,
+        ItemId::DRAGON_FANG,
+        ItemId::HARD_STONE,
+        ItemId::MAGNET,
+        ItemId::METAL_COAT,
+        ItemId::MIRACLE_SEED,
+        ItemId::MYSTIC_WATER,
+        ItemId::NEVER_MELT_ICE,
+        ItemId::POISON_BARB,
+        ItemId::SHARP_BEAK,
+        ItemId::SILK_SCARF,
+        ItemId::SILVER_POWDER,
+        ItemId::SOFT_SAND,
+        ItemId::SPELL_TAG,
+        ItemId::TWISTED_SPOON,
+        ItemId::FAIRY_DUST,
+    ];
+
     public function __construct(
         public readonly string $id,
         public readonly array $trainers,
@@ -27,6 +50,12 @@ final class Area
     public function getClearancePrizes(): array
     {
         $areaConfigEntries = require __DIR__ . "/../../Config/Areas.php";
+
+        if (!array_key_exists($this->id, $areaConfigEntries)) {
+            $hash = crc32($this->id);
+            $index = $hash % count(self::DEFAULT_PRIZES);
+            return self::DEFAULT_PRIZES[$index];
+        }
 
         $areaConfig = $areaConfigEntries[$this->id];
 
